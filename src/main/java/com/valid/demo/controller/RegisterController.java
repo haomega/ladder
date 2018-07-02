848cddad69348654d394777c9c8260a895409e24
luhao <1282103261@qq.com>
2018-07-02 17:30:32 +0800

    UUID

--- src/main/java/com/valid/demo/controller/RegisterController.java
+++ src/main/java/com/valid/demo/controller/RegisterController.java
@@ -1,6 +1,8 @@
-package com.valid.demo.valid;
+package com.valid.demo.controller;
 
 
+import com.valid.demo.valid.BodyValid;
+import com.valid.demo.valid.RegisterForm;
 import com.valid.demo.valid.reps.RepValidError;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @Controller
 public class RegisterController {
+
 
     @GetMapping("")
     public String register() {
         return "register";
     }
 
+    //获取主键
+    public String getUUID() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid;
+    }
+
+    //表单验证
     @PostMapping("")
     public ModelAndView valid(@Valid RegisterForm registerForm, BindingResult result) {
 
@@ -32,5 +43,9 @@ public class RegisterController {
         }
 
     }
+    //TODO: token 令牌的使用
+    //TODO: DAO层添加
+    //TODO: druid德鲁伊
+
 }
 
